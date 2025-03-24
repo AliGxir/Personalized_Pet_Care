917e31aeff36de78ad117c9c780d55790797dcad
AliGxir <147768928+AliGxir@users.noreply.github.com>
2025-03-24 16:42:38 -0600

    set up models for db

---
+++ server/src/models/pet.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    species = db.Column(db.String(50), nullable=False)
+    breed = db.Column(db.String(80), nullable=True)
+    age = db.Column(db.Integer, nullable=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    owner = db.relationship('User', backref='pets')
